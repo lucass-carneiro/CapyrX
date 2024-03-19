662f7c802cba8944b96741ab9ea80a65fd71d505
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2024-03-19 16:29:41 -0300

    MultiPatch: Added more tests to the Two Cubes patch

--- MultiPatch/src/two_cubes/two_cubes.cxx
+++ MultiPatch/src/two_cubes/two_cubes.cxx
@@ -81,6 +81,8 @@ CCTK_DEVICE CCTK_HOST svec local2global(const PatchTransformations &pt,
 
 static inline patch_piece get_owner_patch(const PatchTransformations &pt,
                                           const svec &global_vars) {
+  using MultiPatchTests::isapprox;
+
   const auto x = global_vars(0);
   const auto y = global_vars(0);
   const auto z = global_vars(0);
@@ -92,7 +94,11 @@ static inline patch_piece get_owner_patch(const PatchTransformations &pt,
 
   if (x < x_bnd) {
     return patch_piece::left_cube;
-  } else if (x > x_bnd) {
+  } else if (x > x_bnd || isapprox(x, 0.0)) {
+    /* Note: Here when x = 0, we return the right cube patch as the owner. This
+     * is an arbitrary choice. The x = 0 point is ambiguous but it is never used
+     * in interpatch interpolation, so we can freely choose what to return. This
+     * return is used later when unit tests are being performed.*/
     return patch_piece::right_cube;
   } else {
 #ifndef __CUDACC__
@@ -375,6 +381,116 @@ std::string patch_owner_test(const PatchTransformations &pt,
   return msg.str();
 }
 
+/**
+ * Tests if local2global(global2local(global)) == global
+ *
+ * @param pt The patch transformations structure
+ * @param global_vars A global point to test.
+ * @return A string indicating the test status.
+ */
+std::string
+global_identity_test(const PatchTransformations &pt,
+                     const MultiPatch::TwoCubes::svec &global_vars) {
+
+  using MultiPatchTests::colored;
+  using MultiPatchTests::isapprox;
+  using MultiPatchTests::string_color;
+
+  std::ostringstream msg;
+  msg << "has ";
+
+  const auto g2l = pt.global2local(pt, global_vars);
+
+  const auto owner_patch_idx = std::get<0>(g2l);
+  const auto local_vars = std::get<1>(g2l);
+
+  const auto l2g = pt.local2global(pt, owner_patch_idx, local_vars);
+
+  const auto test1 = isapprox(l2g(0), global_vars(0));
+  const auto test2 = isapprox(l2g(1), global_vars(1));
+  const auto test3 = isapprox(l2g(2), global_vars(2));
+
+  if (test1 && test2 && test3) {
+    msg << MultiPatchTests::PASSED;
+  } else {
+    msg << MultiPatchTests::FAILED << ". Reason: ";
+
+    if (!test1) {
+      msg << l2g(0) << " =/= " << global_vars(0) << ". ";
+    }
+
+    if (!test2) {
+      msg << l2g(1) << " =/= " << global_vars(1) << ". ";
+    }
+
+    if (!test3) {
+      msg << l2g(2) << " =/= " << global_vars(2) << ". ";
+    }
+  }
+
+  return msg.str();
+}
+
+/**
+ * Tests if global2local(local2global(local, patch)) == (local, patch)
+ *
+ * @param pt The patch transformations structure
+ * @param patch The patch index to test.
+ * @param local_point A local point to test.
+ * @return A string indicating the test status.
+ */
+std::string local_identity_test(const PatchTransformations &pt, int patch,
+                                const MultiPatch::TwoCubes::svec &local_point) {
+
+  using MultiPatch::TwoCubes::patch_piece;
+  using MultiPatch::TwoCubes::piece_name;
+  using MultiPatchTests::colored;
+  using MultiPatchTests::isapprox;
+  using MultiPatchTests::string_color;
+
+  std::ostringstream msg;
+  msg << "has ";
+
+  const auto l2g = pt.local2global(pt, patch, local_point);
+  const auto g2l = pt.global2local(pt, l2g);
+
+  const auto computed_patch_idx = std::get<0>(g2l);
+  const auto computed_local_point = std::get<1>(g2l);
+
+  const bool test1 = computed_patch_idx == patch;
+  const bool test2 = isapprox(computed_local_point(0), local_point(0));
+  const bool test3 = isapprox(computed_local_point(1), local_point(1));
+  const bool test4 = isapprox(computed_local_point(2), local_point(2));
+
+  if (test1 && test2 && test3 && test4) {
+    msg << MultiPatchTests::PASSED;
+  } else {
+    msg << MultiPatchTests::FAILED << ". Reason: ";
+
+    if (!test1) {
+      msg << "the computed patch is "
+          << piece_name(static_cast<patch_piece>(computed_patch_idx)) << ". ";
+    }
+
+    if (!test2) {
+      msg << "the computed local coordinate a is " << computed_local_point(0)
+          << ". ";
+    }
+
+    if (!test3) {
+      msg << "the computed local coordinate a is " << computed_local_point(1)
+          << ". ";
+    }
+
+    if (!test4) {
+      msg << "the computed local coordinate a is " << computed_local_point(2)
+          << ". ";
+    }
+  }
+
+  return msg.str();
+}
+
 } // namespace TwoCubesTests
 } // namespace MultiPatch
 
@@ -385,7 +501,15 @@ extern "C" void MultiPatch_run_two_cubes_tests(CCTK_ARGUMENTS) {
   using MultiPatch::SetupTwoCubes;
   using MultiPatch::TwoCubes::patch_piece;
   using MultiPatch::TwoCubes::svec;
+
+  using MultiPatch::TwoCubesTests::global_identity_test;
+  using MultiPatch::TwoCubesTests::local_identity_test;
   using MultiPatch::TwoCubesTests::patch_owner_test;
+
+  using MultiPatchTests::random_seed;
+  using std::mt19937_64;
+  using std::uniform_int_distribution;
+  using std::uniform_real_distribution;
 
   const auto ps{SetupTwoCubes()};
   const auto &pt{ps.transformations};
@@ -402,21 +526,109 @@ extern "C" void MultiPatch_run_two_cubes_tests(CCTK_ARGUMENTS) {
   const auto zmax{pt.two_cubes_zmax};
   const auto zmid{zmin + (zmax - zmin) / 2};
 
+  const auto xcent_l{xmin + (xmax - xmin) / 4.0};
+  const auto xcent_r{xmax - (xmax - xmin) / 4.0};
+
   CCTK_INFO("Running Two Cubes patch tests:");
 
   /*
    * Fixed point tests
    */
-  const std::array<std::pair<svec, patch_piece>, 2> owner_test_data = {
-      std::make_pair(svec{xmin + xmid / 2.0, ymid, zmid},
-                     patch_piece::left_cube),
-      std::make_pair(svec{xmax - xmid / 2.0, ymid, zmid},
-                     patch_piece::right_cube)};
+  const std::array<std::pair<svec, patch_piece>, 35> owner_test_data = {
+      // X variation
+      std::make_pair(svec{xmin, ymid, zmid}, patch_piece::left_cube),
+      std::make_pair(svec{xcent_l, ymid, zmid}, patch_piece::left_cube),
+      std::make_pair(svec{xmid, ymid, zmid}, patch_piece::right_cube),
+      std::make_pair(svec{xcent_r, ymid, zmid}, patch_piece::right_cube),
+      std::make_pair(svec{xmax, ymid, zmid}, patch_piece::right_cube),
+
+      // Y variation left
+      std::make_pair(svec{xcent_l, ymin, zmid}, patch_piece::left_cube),
+      std::make_pair(svec{xcent_l, ymid, zmid}, patch_piece::left_cube),
+      std::make_pair(svec{xcent_l, ymax, zmid}, patch_piece::left_cube),
+
+      // Y variation right
+      std::make_pair(svec{xcent_r, ymin, zmid}, patch_piece::right_cube),
+      std::make_pair(svec{xcent_r, ymid, zmid}, patch_piece::right_cube),
+      std::make_pair(svec{xcent_r, ymax, zmid}, patch_piece::right_cube),
+
+      // Z variation left
+      std::make_pair(svec{xcent_l, ymid, zmin}, patch_piece::left_cube),
+      std::make_pair(svec{xcent_l, ymid, zmid}, patch_piece::left_cube),
+      std::make_pair(svec{xcent_l, ymid, zmax}, patch_piece::left_cube),
+
+      // Z variation right
+      std::make_pair(svec{xcent_r, ymid, zmin}, patch_piece::right_cube),
+      std::make_pair(svec{xcent_r, ymid, zmid}, patch_piece::right_cube),
+      std::make_pair(svec{xcent_r, ymid, zmax}, patch_piece::right_cube),
+
+      // Corners
+      std::make_pair(svec{xmax, ymax, zmax}, patch_piece::right_cube),
+      std::make_pair(svec{xmax, ymin, zmax}, patch_piece::right_cube),
+      std::make_pair(svec{xmin, ymin, zmax}, patch_piece::left_cube),
+      std::make_pair(svec{xmin, ymax, zmax}, patch_piece::left_cube),
+
+      std::make_pair(svec{xmax, ymax, zmin}, patch_piece::right_cube),
+      std::make_pair(svec{xmax, ymin, zmin}, patch_piece::right_cube),
+      std::make_pair(svec{xmin, ymin, zmin}, patch_piece::left_cube),
+      std::make_pair(svec{xmin, ymax, zmin}, patch_piece::left_cube),
+
+      // Exteriors
+      std::make_pair(svec{xcent_l, ymid, zmax + 1.0}, patch_piece::left_cube),
+      std::make_pair(svec{xcent_l, ymid, zmin - 1.0}, patch_piece::left_cube),
+      std::make_pair(svec{xcent_l, ymax + 1, zmid}, patch_piece::left_cube),
+      std::make_pair(svec{xcent_l, ymin - 1, zmid}, patch_piece::left_cube),
+      std::make_pair(svec{xmin - 1, ymid, zmid}, patch_piece::left_cube),
+
+      std::make_pair(svec{xcent_r, ymid, zmax + 1.0}, patch_piece::right_cube),
+      std::make_pair(svec{xcent_r, ymid, zmin - 1.0}, patch_piece::right_cube),
+      std::make_pair(svec{xcent_r, ymax + 1, zmid}, patch_piece::right_cube),
+      std::make_pair(svec{xcent_r, ymin - 1, zmid}, patch_piece::right_cube),
+      std::make_pair(svec{xmax + 1, ymid, zmid}, patch_piece::right_cube)};
 
   // Tests if the patch owner is correct.
   for (const auto &data : owner_test_data) {
     CCTK_VINFO("  Patch owner test at point (%f, %f, %f) %s", data.first(0),
                data.first(1), data.first(2),
                patch_owner_test(pt, data.first, data.second).c_str());
+  }
+
+  /*
+   * Random point tests
+   */
+  mt19937_64 engine(random_seed);
+
+  uniform_real_distribution<CCTK_REAL> x_distrib(xmin, xmax);
+  uniform_real_distribution<CCTK_REAL> y_distrib(ymin, ymax);
+  uniform_real_distribution<CCTK_REAL> z_distrib(zmin, zmax);
+  uniform_real_distribution<CCTK_REAL> local_distrib(-1, 1);
+  uniform_int_distribution<int> patch_distrib(0, 1);
+
+  auto global_point = svec{0, 0, 0};
+  auto local_point = svec{0, 0, 0};
+  int patch = 0;
+
+  // Tests if local2global(global2local(global)) == global
+  for (int i = 0; i < test_repetitions; i++) {
+    global_point = {x_distrib(engine), y_distrib(engine), z_distrib(engine)};
+
+    CCTK_VINFO("  local2global(global2local(global)) transformation test at "
+               "point (%f, %f, %f) %s",
+               global_point(0), global_point(1), global_point(2),
+               global_identity_test(pt, global_point).c_str());
+  }
+
+  // Tests if global2local(local2global(local, patch)) == (local, patch)
+  for (int i = 0; i < test_repetitions; i++) {
+    local_point = {local_distrib(engine), local_distrib(engine),
+                   local_distrib(engine)};
+
+    patch = patch_distrib(engine);
+
+    CCTK_VINFO("  global2local(local2global(local, patch)) transformation test "
+               "at point (%f, %f, %f) patch %s %s",
+               local_point(0), local_point(1), local_point(2),
+               piece_name(static_cast<patch_piece>(patch)),
+               local_identity_test(pt, patch, local_point).c_str());
   }
 }

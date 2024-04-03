81bba3d00ca2bb1ee46f5380fa4fb444b27b5f21
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2024-04-03 20:42:35 -0300

    MultipatchWaveToy: Restored error computation. Reorganized parameter files

--- MultiPatchWaveToy/src/initial.cxx
+++ MultiPatchWaveToy/src/initial.cxx
@@ -3,19 +3,16 @@
 #include <cmath>
 #include <limits>
 
-namespace MultiPatchWaveToyX {
+namespace MultiPatchWaveToy {
 
-/*
- * u(t,x,y,z) = A cos(2 pi omega t) sin(2 pi kx x) sin(2 pi ky y) sin(2 pi kz z)
- */
-template <typename T>
-constexpr void standing_wave(const T A, const T kx, const T ky, const T kz,
-                             const T t, const T x, const T y, const T z, T &u,
-                             T &rho) {
+void standing_wave(const CCTK_REAL A, const CCTK_REAL kx, const CCTK_REAL ky,
+                   const CCTK_REAL kz, const CCTK_REAL t, const CCTK_REAL x,
+                   const CCTK_REAL y, const CCTK_REAL z, CCTK_REAL &u,
+                   CCTK_REAL &rho) noexcept {
   using std::acos, std::cos, std::pow, std::sin, std::sqrt;
 
-  const T pi = acos(-T(1));
-  const T omega = sqrt(pow(kx, 2) + pow(ky, 2) + pow(kz, 2));
+  const CCTK_REAL pi = acos(-CCTK_REAL(1));
+  const CCTK_REAL omega = sqrt(pow(kx, 2) + pow(ky, 2) + pow(kz, 2));
 
   u = A * cos(2 * pi * omega * t) * cos(2 * pi * kx * x) *
       cos(2 * pi * ky * y) * cos(2 * pi * kz * z);
@@ -23,22 +20,18 @@ constexpr void standing_wave(const T A, const T kx, const T ky, const T kz,
         cos(2 * pi * ky * y) * cos(2 * pi * kz * z);
 }
 
-/*
- * u(t,r) = (f(t-r) - f(t+r)) / r
- * f(v) = A exp(-1/2 (r/W)^2)
- */
-template <typename T>
-constexpr void gaussian(const T A, const T W, const T t, const T x, const T y,
-                        const T z, T &u, T &rho) {
+void gaussian(const CCTK_REAL A, const CCTK_REAL W, const CCTK_REAL t,
+              const CCTK_REAL x, const CCTK_REAL y, const CCTK_REAL z,
+              CCTK_REAL &u, CCTK_REAL &rho) noexcept {
   using std::exp, std::pow, std::sqrt;
 
-  const T r = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+  const CCTK_REAL r = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
 
-  const auto f = [&](const T v) {
+  const auto f = [&](const CCTK_REAL v) {
     return A * exp(-pow(v, 2) / (2 * pow(W, 2)));
   };
 
-  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+  if (r < sqrt(std::numeric_limits<CCTK_REAL>::epsilon())) {
     // L'Hôpital
     u = 2 / pow(W, 2) * f(t) * t;
     rho = -2 / pow(W, 4) * f(t) * (pow(t, 2) - pow(W, 2));
@@ -74,4 +67,4 @@ extern "C" void MultiPatchWaveToy_Initial(CCTK_ARGUMENTS) {
   }
 }
 
-} // namespace MultiPatchWaveToyX
+} // namespace MultiPatchWaveToy
--- MultiPatchWaveToy/src/mpwavetoy.hxx
+++ MultiPatchWaveToy/src/mpwavetoy.hxx
@@ -13,14 +13,28 @@
 
 namespace MultiPatchWaveToy {
 
-constexpr const int dim = 3;
+constexpr int dim{3};
+
+/*
+ * u(t,x,y,z) = A cos(2 pi omega t) sin(2 pi kx x) sin(2 pi ky y) sin(2 pi kz z)
+ */
+void standing_wave(const CCTK_REAL A, const CCTK_REAL kx, const CCTK_REAL ky, const CCTK_REAL kz,
+                             const CCTK_REAL t, const CCTK_REAL x, const CCTK_REAL y, const CCTK_REAL z, CCTK_REAL &u,
+                             CCTK_REAL &rho) noexcept;
+                             
+/*
+ * u(t,r) = (f(t-r) - f(t+r)) / r
+ * f(v) = A exp(-1/2 (r/W)^2)
+ */
+void gaussian(const CCTK_REAL A, const CCTK_REAL W, const CCTK_REAL t, const CCTK_REAL x, const CCTK_REAL y,
+                        const CCTK_REAL z, CCTK_REAL &u, CCTK_REAL &rho) noexcept;
 
 extern "C" {
 
 void MultiPatchWaveToy_Initial(CCTK_ARGUMENTS);
 void MultiPatchWaveToy_RHS(CCTK_ARGUMENTS);
-void MultiPatchWaveToy_Energy(CCTK_ARGUMENTS);
 void MultiPatchWaveToy_Error(CCTK_ARGUMENTS);
+
 }
 
 } // namespace MultiPatchWaveToy

a7f0a5eff4efc149cf7f43deb8c247a79ba3382c
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2024-07-03 17:17:27 -0300

    MultiPatch: Updated owner code for the Cake patch

--- MultiPatch/src/cake/cake_get_owner_patch.cxx
+++ MultiPatch/src/cake/cake_get_owner_patch.cxx
@@ -3,6 +3,7 @@
 #include <array>
 #include <algorithm>
 #include <cassert>
+#include <cmath>
 
 /**
  * @brief Get the patch piece that owns a global coordinate point.
@@ -14,52 +15,50 @@
 CCTK_DEVICE CCTK_HOST MultiPatch::Cake::patch_piece
 MultiPatch::Cake::get_owner_patch(const PatchTransformations &pt,
                                   const svec &global_vars) {
-  using std::abs;
   using std::distance;
+  using std::fabs;
   using std::max_element;
 
-  const auto x = global_vars(0);
-  const auto y = global_vars(1);
-  const auto z = global_vars(2);
+  const auto x{global_vars(0)};
+  const auto y{global_vars(1)};
+  const auto z{global_vars(2)};
+
+  const auto abs_x{fabs(x)};
+  const auto abs_y{fabs(y)};
+  const auto abs_z{fabs(z)};
 
   const auto r0 = pt.cake_inner_boundary_radius;
 
-  const bool x_within_r0{-r0 < x && x < r0};
-  const bool y_within_r0{-r0 < y && y < r0};
-  const bool z_within_r0{-r0 < z && z < r0};
-
-  // Are we on the Cartesian core?
-  if (x_within_r0 && y_within_r0 && z_within_r0) {
+  if (abs_x <= r0 && abs_y <= r0 && abs_z <= r0) {
     return patch_piece::cartesian;
   }
 
-  /* We are on one of the 6 Thornburg patches. The patch direction is the
-   * determined by the coordinate with the largest absolute value
-   */
-  const std::array<CCTK_REAL, 3> abs_global_coords{abs(x), abs(y), abs(z)};
-  const auto max_abs_global_coord_idx{distance(
-      abs_global_coords.begin(),
-      max_element(abs_global_coords.begin(), abs_global_coords.end()))};
+  std::array<CCTK_REAL, 3> abs_coords{abs_x, abs_y, abs_z};
+  const auto max_coord_idx{distance(
+      abs_coords.begin(), max_element(abs_coords.begin(), abs_coords.end()))};
 
-  /* After the direction is know, we must dtermine the sense, which is
-   * represented by the sign of the coordinate with the largest absolute
-   * value.
-   */
-  const int max_coord_sign{global_vars(max_abs_global_coord_idx) > 0.0 ? 1
-                                                                       : -1};
-
-  if (max_coord_sign < 0 && max_abs_global_coord_idx == 0) {
-    return patch_piece::minus_x;
-  } else if (max_coord_sign > 0 && max_abs_global_coord_idx == 0) {
+  if (x > 0.0 && max_coord_idx == 0) {
     return patch_piece::plus_x;
-  } else if (max_coord_sign < 0 && max_abs_global_coord_idx == 1) {
-    return patch_piece::minus_y;
-  } else if (max_coord_sign > 0 && max_abs_global_coord_idx == 1) {
+  }
+
+  if (x < 0.0 && max_coord_idx == 0) {
+    return patch_piece::minus_x;
+  }
+
+  if (y > 0.0 && max_coord_idx == 1) {
     return patch_piece::plus_y;
-  } else if (max_coord_sign < 0 && max_abs_global_coord_idx == 2) {
-    return patch_piece::minus_z;
-  } else if (max_coord_sign > 0 && max_abs_global_coord_idx == 2) {
+  }
+
+  if (y < 0.0 && max_coord_idx == 1) {
+    return patch_piece::minus_y;
+  }
+
+  if (z > 0.0 && max_coord_idx == 2) {
     return patch_piece::plus_z;
+  }
+
+  if (z < 0.0 && max_coord_idx == 2) {
+    return patch_piece::minus_z;
   }
 
 // We don't know where we are. This is unexpected
--- MultiPatch/src/cake/cake_tests.cxx
+++ MultiPatch/src/cake/cake_tests.cxx
@@ -486,146 +486,147 @@ extern "C" void MultiPatch_run_cake_tests(CCTK_ARGUMENTS) {
   /*
    * Fixed point tests
    */
-  const std::array<std::pair<svec, patch_piece>, 36> owner_test_data = {
-      // Cartesian cube interior
-      std::make_pair(svec{cube_midpoint, cube_midpoint, cube_midpoint},
-                     patch_piece::cartesian),
-      std::make_pair(svec{-cube_midpoint, cube_midpoint, cube_midpoint},
-                     patch_piece::cartesian),
-      std::make_pair(svec{cube_midpoint, -cube_midpoint, cube_midpoint},
-                     patch_piece::cartesian),
-      std::make_pair(svec{cube_midpoint, cube_midpoint, -cube_midpoint},
-                     patch_piece::cartesian),
+  const std::array<std::pair<svec, patch_piece>, 37> owner_test_data = {
+    // Cartesian cube interior
+    std::make_pair(svec{cube_midpoint, cube_midpoint, cube_midpoint},
+                   patch_piece::cartesian),
+    std::make_pair(svec{-cube_midpoint, cube_midpoint, cube_midpoint},
+                   patch_piece::cartesian),
+    std::make_pair(svec{cube_midpoint, -cube_midpoint, cube_midpoint},
+                   patch_piece::cartesian),
+    std::make_pair(svec{cube_midpoint, cube_midpoint, -cube_midpoint},
+                   patch_piece::cartesian),
 
-      // Cartesian cube corners
-      std::make_pair(svec{r0, r0, r0}, patch_piece::plus_x),
-      std::make_pair(svec{r0, -r0, r0}, patch_piece::plus_x),
-      std::make_pair(svec{-r0, -r0, r0}, patch_piece::minus_x),
-      std::make_pair(svec{-r0, r0, r0}, patch_piece::minus_x),
+    // Cartesian cube corners
+    std::make_pair(svec{r0, r0, r0}, patch_piece::cartesian),
+    std::make_pair(svec{r0, -r0, r0}, patch_piece::cartesian),
+    std::make_pair(svec{-r0, -r0, r0}, patch_piece::cartesian),
+    std::make_pair(svec{-r0, r0, r0}, patch_piece::cartesian),
 
-      std::make_pair(svec{r0, r0, -r0}, patch_piece::plus_x),
-      std::make_pair(svec{r0, -r0, -r0}, patch_piece::plus_x),
-      std::make_pair(svec{-r0, -r0, -r0}, patch_piece::minus_x),
-      std::make_pair(svec{-r0, r0, -r0}, patch_piece::minus_x),
+    std::make_pair(svec{r0, r0, -r0}, patch_piece::cartesian),
+    std::make_pair(svec{r0, -r0, -r0}, patch_piece::cartesian),
+    std::make_pair(svec{-r0, -r0, -r0}, patch_piece::cartesian),
+    std::make_pair(svec{-r0, r0, -r0}, patch_piece::cartesian),
 
-      // Middle of the +x patch
-      std::make_pair(svec{r0, 0, 0}, patch_piece::plus_x),
-      std::make_pair(svec{patch_midpoint, 0, 0}, patch_piece::plus_x),
-      std::make_pair(svec{r1, 0, 0}, patch_piece::plus_x),
+    // Middle of the +x patch
+    std::make_pair(svec{r0, 0, 0}, patch_piece::cartesian),
+    std::make_pair(svec{patch_midpoint, 0, 0}, patch_piece::plus_x),
+    std::make_pair(svec{r1, 0, 0}, patch_piece::plus_x),
 
-      // Middle of the -x patch
-      std::make_pair(svec{-r0, 0, 0}, patch_piece::minus_x),
-      std::make_pair(svec{-patch_midpoint, 0, 0}, patch_piece::minus_x),
-      std::make_pair(svec{-r1, 0, 0}, patch_piece::minus_x),
+    // Middle of the -x patch
+    std::make_pair(svec{-r0, 0, 0}, patch_piece::cartesian),
+    std::make_pair(svec{-patch_midpoint, 0, 0}, patch_piece::minus_x),
+    std::make_pair(svec{-r1, 0, 0}, patch_piece::minus_x),
 
-      // Middle of the +y patch
-      std::make_pair(svec{0, r0, 0}, patch_piece::plus_y),
-      std::make_pair(svec{0, patch_midpoint, 0}, patch_piece::plus_y),
-      std::make_pair(svec{0, r1, 0}, patch_piece::plus_y),
+    // Middle of the +y patch
+    std::make_pair(svec{0, r0, 0}, patch_piece::cartesian),
+    std::make_pair(svec{0, patch_midpoint, 0}, patch_piece::plus_y),
+    std::make_pair(svec{0, r1, 0}, patch_piece::plus_y),
 
-      // Middle of the -y patch
-      std::make_pair(svec{0, -r0, 0}, patch_piece::minus_y),
-      std::make_pair(svec{0, -patch_midpoint, 0}, patch_piece::minus_y),
-      std::make_pair(svec{0, -r1, 0}, patch_piece::minus_y),
+    // Middle of the -y patch
+    std::make_pair(svec{0, -r0, 0}, patch_piece::cartesian),
+    std::make_pair(svec{0, -patch_midpoint, 0}, patch_piece::minus_y),
+    std::make_pair(svec{0, -r1, 0}, patch_piece::minus_y),
 
-      // Middle of the +z patch
-      std::make_pair(svec{0, 0, r0}, patch_piece::plus_z),
-      std::make_pair(svec{0, 0, patch_midpoint}, patch_piece::plus_z),
-      std::make_pair(svec{0, 0, r1}, patch_piece::plus_z),
+    // Middle of the +z patch
+    std::make_pair(svec{0, 0, r0}, patch_piece::cartesian),
+    std::make_pair(svec{0, 0, patch_midpoint}, patch_piece::plus_z),
+    std::make_pair(svec{0, 0, r1}, patch_piece::plus_z),
 
-      // Middle of the -z patch
-      std::make_pair(svec{0, 0, -r0}, patch_piece::minus_z),
-      std::make_pair(svec{0, 0, -patch_midpoint}, patch_piece::minus_z),
-      std::make_pair(svec{0, 0, -r1}, patch_piece::minus_z),
+    // Middle of the -z patch
+    std::make_pair(svec{0, 0, -r0}, patch_piece::cartesian),
+    std::make_pair(svec{0, 0, -patch_midpoint}, patch_piece::minus_z),
+    std::make_pair(svec{0, 0, -r1}, patch_piece::minus_z),
 
-      // Exterior
-      std::make_pair(svec{r1 + 1, 0, 0}, patch_piece::plus_x),
-      std::make_pair(svec{0, r1 + 1, 0}, patch_piece::plus_y),
-      std::make_pair(svec{0, 0, r1 + 1}, patch_piece::plus_z),
-      std::make_pair(svec{-(r1 + 1), 0, 0}, patch_piece::minus_x),
-      std::make_pair(svec{0, -(r1 + 1), 0}, patch_piece::minus_y),
-      std::make_pair(svec{0, 0, -(r1 + 1)}, patch_piece::minus_z)};
+    // Exterior
+    std::make_pair(svec{r1 + 1, 0, 0}, patch_piece::plus_x),
+    std::make_pair(svec{0, r1 + 1, 0}, patch_piece::plus_y),
+    std::make_pair(svec{0, 0, r1 + 1}, patch_piece::plus_z),
+    std::make_pair(svec{-(r1 + 1), 0, 0}, patch_piece::minus_x),
+    std::make_pair(svec{0, -(r1 + 1), 0}, patch_piece::minus_y),
+    std::make_pair(svec{0, 0, -(r1 + 1)}, patch_piece::minus_z),
 
-  // Tests if the patch owner is correct.
-  for (const auto &data : owner_test_data) {
-    CCTK_VINFO("  Patch owner test at point (%f, %f, %f) %s", data.first(0),
-               data.first(1), data.first(2),
-               patch_owner_test(pt, data.first, data.second).c_str());
-  }
+    // Tests if the patch owner is correct.
+    for (const auto &data
+         : owner_test_data){
+        CCTK_VINFO("  Patch owner test at point (%f, %f, %f) %s", data.first(0),
+                   data.first(1), data.first(2),
+                   patch_owner_test(pt, data.first, data.second).c_str());
+}
 
-  /*
-   * Random point tests
-   */
-  mt19937_64 engine(random_seed);
+/*
+ * Random point tests
+ */
+mt19937_64 engine(random_seed);
 
-  uniform_real_distribution<CCTK_REAL> r_distrib(0, r1);
-  uniform_real_distribution<CCTK_REAL> theta_distrib(0, CCTK_REAL(M_PI));
-  uniform_real_distribution<CCTK_REAL> phi_distrib(0, 2 * CCTK_REAL(M_PI));
+uniform_real_distribution<CCTK_REAL> r_distrib(0, r1);
+uniform_real_distribution<CCTK_REAL> theta_distrib(0, CCTK_REAL(M_PI));
+uniform_real_distribution<CCTK_REAL> phi_distrib(0, 2 * CCTK_REAL(M_PI));
 
-  uniform_real_distribution<CCTK_REAL> local_distrib(-1, 1);
+uniform_real_distribution<CCTK_REAL> local_distrib(-1, 1);
 
-  uniform_int_distribution<int> patch_distrib(0, 6);
+uniform_int_distribution<int> patch_distrib(0, 6);
 
-  auto global_point = svec{0, 0, 0};
-  auto local_point = svec{0, 0, 0};
-  CCTK_REAL r = 0, theta = 0, phi = 0;
-  int patch = 0;
+auto global_point = svec{0, 0, 0};
+auto local_point = svec{0, 0, 0};
+CCTK_REAL r = 0, theta = 0, phi = 0;
+int patch = 0;
 
-  // Tests if local2global(global2local(global)) == global
-  for (int i = 0; i < test_repetitions; i++) {
-    r = r_distrib(engine);
-    theta = theta_distrib(engine);
-    phi = phi_distrib(engine);
+// Tests if local2global(global2local(global)) == global
+for (int i = 0; i < test_repetitions; i++) {
+  r = r_distrib(engine);
+  theta = theta_distrib(engine);
+  phi = phi_distrib(engine);
 
-    global_point = {r * sin(theta) * cos(phi), r * sin(theta) * sin(phi),
-                    r * cos(theta)};
+  global_point = {r * sin(theta) * cos(phi), r * sin(theta) * sin(phi),
+                  r * cos(theta)};
 
-    CCTK_VINFO("  local2global(global2local(global)) transformation test at "
-               "point (%f, %f, %f) %s",
-               global_point(0), global_point(1), global_point(2),
-               global_identity_test(pt, global_point).c_str());
-  }
+  CCTK_VINFO("  local2global(global2local(global)) transformation test at "
+             "point (%f, %f, %f) %s",
+             global_point(0), global_point(1), global_point(2),
+             global_identity_test(pt, global_point).c_str());
+}
 
-  // Tests if global2local(local2global(local, patch)) == (local, patch)
-  for (int i = 0; i < test_repetitions; i++) {
-    local_point = {local_distrib(engine), local_distrib(engine),
-                   local_distrib(engine)};
+// Tests if global2local(local2global(local, patch)) == (local, patch)
+for (int i = 0; i < test_repetitions; i++) {
+  local_point = {local_distrib(engine), local_distrib(engine),
+                 local_distrib(engine)};
 
-    patch = patch_distrib(engine);
+  patch = patch_distrib(engine);
 
-    CCTK_VINFO("  global2local(local2global(local, patch)) transformation test "
-               "at point (%f, %f, %f) patch %s %s",
-               local_point(0), local_point(1), local_point(2),
-               piece_name(static_cast<patch_piece>(patch)).c_str(),
-               local_identity_test(pt, patch, local_point).c_str());
-  }
+  CCTK_VINFO("  global2local(local2global(local, patch)) transformation test "
+             "at point (%f, %f, %f) patch %s %s",
+             local_point(0), local_point(1), local_point(2),
+             piece_name(static_cast<patch_piece>(patch)).c_str(),
+             local_identity_test(pt, patch, local_point).c_str());
+}
 
-  // Tests if local -> global jacobians are correct.
-  for (int i = 0; i < test_repetitions; i++) {
-    r = r_distrib(engine);
-    theta = theta_distrib(engine);
-    phi = phi_distrib(engine);
+// Tests if local -> global jacobians are correct.
+for (int i = 0; i < test_repetitions; i++) {
+  r = r_distrib(engine);
+  theta = theta_distrib(engine);
+  phi = phi_distrib(engine);
 
-    global_point = {r * sin(theta) * cos(phi), r * sin(theta) * sin(phi),
-                    r * cos(theta)};
+  global_point = {r * sin(theta) * cos(phi), r * sin(theta) * sin(phi),
+                  r * cos(theta)};
 
-    CCTK_VINFO("  local -> global jacobian test at point (%f, %f, %f) patch %s",
-               global_point(0), global_point(1), global_point(2),
-               jacobian_test(pt, patch, global_point).c_str());
-  }
+  CCTK_VINFO("  local -> global jacobian test at point (%f, %f, %f) patch %s",
+             global_point(0), global_point(1), global_point(2),
+             jacobian_test(pt, patch, global_point).c_str());
+}
 
-  // Tests if local -> global jacobian derivatives are correct.
-  for (int i = 0; i < test_repetitions; i++) {
-    r = r_distrib(engine);
-    theta = theta_distrib(engine);
-    phi = phi_distrib(engine);
+// Tests if local -> global jacobian derivatives are correct.
+for (int i = 0; i < test_repetitions; i++) {
+  r = r_distrib(engine);
+  theta = theta_distrib(engine);
+  phi = phi_distrib(engine);
 
-    global_point = {r * sin(theta) * cos(phi), r * sin(theta) * sin(phi),
-                    r * cos(theta)};
+  global_point = {r * sin(theta) * cos(phi), r * sin(theta) * sin(phi),
+                  r * cos(theta)};
 
-    CCTK_VINFO("  local -> global jacobian derivative test at point (%f, %f, "
-               "%f) patch %s",
-               global_point(0), global_point(1), global_point(2),
-               djacobian_test(pt, patch, global_point).c_str());
-  }
+  CCTK_VINFO("  local -> global jacobian derivative test at point (%f, %f, "
+             "%f) patch %s",
+             global_point(0), global_point(1), global_point(2),
+             djacobian_test(pt, patch, global_point).c_str());
+}
 }

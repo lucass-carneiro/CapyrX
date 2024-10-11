8692a41d8c5267c614aba64331085e2501e004e1
Lucas Sanches <lsanches@melete05.cct.lsu.edu>
2024-10-11 17:26:01 -0500

    MultipatchWaveToy: Added the option to compute exact derivatives on the RHS instead of doing FDs and projections. This is usefull for testing projections

--- MultiPatchWaveToy/src/rhs.cxx
+++ MultiPatchWaveToy/src/rhs.cxx
@@ -6,6 +6,8 @@
 #include <cctk_Parameters.h>
 // clang-format on
 
+#include "standing_wave.hxx"
+
 namespace MultiPatchWaveToy {
 
 using namespace Arith;
@@ -30,41 +32,77 @@ extern "C" void MultiPatchWaveToy_RHS(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTSX_MultiPatchWaveToy_RHS;
   DECLARE_CCTK_PARAMETERS;
 
-  using std::pow;
+  if (use_exact_derivatives) {
+    if (CCTK_EQUALS(initial_condition, "standing wave")) {
+      grid.loop_int<0, 0, 0>(
+          grid.nghostzones,
+          [=] CCTK_DEVICE(const Loop::PointDesc &p)
+              CCTK_ATTRIBUTE_ALWAYS_INLINE {
+                const auto t{cctk_time};
+                const auto x{vcoordx(p.I)};
+                const auto y{vcoordy(p.I)};
+                const auto z{vcoordz(p.I)};
 
-  grid.loop_int<0, 0, 0>(
-      grid.nghostzones,
-      [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-        const auto d_Pi_dx{vJ_da_dx(p.I) * fd_l_1_4<local_fd_dir::a>(p, Pi) +
-                           vJ_db_dx(p.I) * fd_l_1_4<local_fd_dir::b>(p, Pi) +
-                           vJ_dc_dx(p.I) * fd_l_1_4<local_fd_dir::c>(p, Pi)};
+                const auto A{amplitude};
+                const auto kx{wave_kx};
+                const auto ky{wave_ky};
+                const auto kz{wave_kz};
 
-        const auto d_Pi_dy{vJ_da_dy(p.I) * fd_l_1_4<local_fd_dir::a>(p, Pi) +
-                           vJ_db_dy(p.I) * fd_l_1_4<local_fd_dir::b>(p, Pi) +
-                           vJ_dc_dy(p.I) * fd_l_1_4<local_fd_dir::c>(p, Pi)};
+                const auto dPidx{sw::dPidx(A, kx, ky, kz, t, x, y, z)};
+                const auto dPidy{sw::dPidy(A, kx, ky, kz, t, x, y, z)};
+                const auto dPidz{sw::dPidz(A, kx, ky, kz, t, x, y, z)};
 
-        const auto d_Pi_dz{vJ_da_dz(p.I) * fd_l_1_4<local_fd_dir::a>(p, Pi) +
-                           vJ_db_dz(p.I) * fd_l_1_4<local_fd_dir::b>(p, Pi) +
-                           vJ_dc_dz(p.I) * fd_l_1_4<local_fd_dir::c>(p, Pi)};
+                const auto dDxdx{sw::dDxdx(A, kx, ky, kz, t, x, y, z)};
+                const auto dDydy{sw::dDydy(A, kx, ky, kz, t, x, y, z)};
+                const auto dDzdz{sw::dDzdz(A, kx, ky, kz, t, x, y, z)};
 
-        const auto d_Dx_dx{vJ_da_dx(p.I) * fd_l_1_4<local_fd_dir::a>(p, Dx) +
-                           vJ_db_dx(p.I) * fd_l_1_4<local_fd_dir::b>(p, Dx) +
-                           vJ_dc_dx(p.I) * fd_l_1_4<local_fd_dir::c>(p, Dx)};
+                phi_rhs(p.I) = Pi(p.I);
+                Pi_rhs(p.I) = dDxdx + dDydy + dDzdz;
+                Dx_rhs(p.I) = dPidx;
+                Dy_rhs(p.I) = dPidy;
+                Dz_rhs(p.I) = dPidz;
+              });
+    } else if (CCTK_EQUALS(initial_condition, "standing wave")) {
+      CCTK_VERROR(
+          "Exact derivativ;e not implemented yet for this initial condition");
+    } else {
+      CCTK_VERROR("Unrecognized initial data type");
+    }
+  } else {
+    grid.loop_int<0, 0, 0>(
+        grid.nghostzones,
+        [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+          const auto d_Pi_dx{vJ_da_dx(p.I) * fd_l_1_4<local_fd_dir::a>(p, Pi) +
+                             vJ_db_dx(p.I) * fd_l_1_4<local_fd_dir::b>(p, Pi) +
+                             vJ_dc_dx(p.I) * fd_l_1_4<local_fd_dir::c>(p, Pi)};
 
-        const auto d_Dy_dy{vJ_da_dy(p.I) * fd_l_1_4<local_fd_dir::a>(p, Dy) +
-                           vJ_db_dy(p.I) * fd_l_1_4<local_fd_dir::b>(p, Dy) +
-                           vJ_dc_dy(p.I) * fd_l_1_4<local_fd_dir::c>(p, Dy)};
+          const auto d_Pi_dy{vJ_da_dy(p.I) * fd_l_1_4<local_fd_dir::a>(p, Pi) +
+                             vJ_db_dy(p.I) * fd_l_1_4<local_fd_dir::b>(p, Pi) +
+                             vJ_dc_dy(p.I) * fd_l_1_4<local_fd_dir::c>(p, Pi)};
 
-        const auto d_Dz_dz{vJ_da_dz(p.I) * fd_l_1_4<local_fd_dir::a>(p, Dz) +
-                           vJ_db_dz(p.I) * fd_l_1_4<local_fd_dir::b>(p, Dz) +
-                           vJ_dc_dz(p.I) * fd_l_1_4<local_fd_dir::c>(p, Dz)};
+          const auto d_Pi_dz{vJ_da_dz(p.I) * fd_l_1_4<local_fd_dir::a>(p, Pi) +
+                             vJ_db_dz(p.I) * fd_l_1_4<local_fd_dir::b>(p, Pi) +
+                             vJ_dc_dz(p.I) * fd_l_1_4<local_fd_dir::c>(p, Pi)};
 
-        phi_rhs(p.I) = Pi(p.I);
-        Pi_rhs(p.I) = d_Dx_dx + d_Dy_dy + d_Dz_dz;
-        Dx_rhs(p.I) = d_Pi_dx;
-        Dy_rhs(p.I) = d_Pi_dy;
-        Dz_rhs(p.I) = d_Pi_dz;
-      });
+          const auto d_Dx_dx{vJ_da_dx(p.I) * fd_l_1_4<local_fd_dir::a>(p, Dx) +
+                             vJ_db_dx(p.I) * fd_l_1_4<local_fd_dir::b>(p, Dx) +
+                             vJ_dc_dx(p.I) * fd_l_1_4<local_fd_dir::c>(p, Dx)};
+
+          const auto d_Dy_dy{vJ_da_dy(p.I) * fd_l_1_4<local_fd_dir::a>(p, Dy) +
+                             vJ_db_dy(p.I) * fd_l_1_4<local_fd_dir::b>(p, Dy) +
+                             vJ_dc_dy(p.I) * fd_l_1_4<local_fd_dir::c>(p, Dy)};
+
+          const auto d_Dz_dz{vJ_da_dz(p.I) * fd_l_1_4<local_fd_dir::a>(p, Dz) +
+                             vJ_db_dz(p.I) * fd_l_1_4<local_fd_dir::b>(p, Dz) +
+                             vJ_dc_dz(p.I) * fd_l_1_4<local_fd_dir::c>(p, Dz)};
+
+          phi_rhs(p.I) = Pi(p.I);
+          Pi_rhs(p.I) = d_Dx_dx + d_Dy_dy + d_Dz_dz;
+          Dx_rhs(p.I) = d_Pi_dx;
+          Dy_rhs(p.I) = d_Pi_dy;
+          Dz_rhs(p.I) = d_Pi_dz;
+        });
+  }
 }
 
 extern "C" void MultiPatchWaveToy_Sync(CCTK_ARGUMENTS) {

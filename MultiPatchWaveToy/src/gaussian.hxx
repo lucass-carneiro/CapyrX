2d1b4173a4f8c180ec150cb5b0e3a7ad8e883312
Lucas <lucas.t.s.carneiro@gmail.com>
2024-10-17 15:54:55 -0500

    MultiPatchWaveToy: Updated initial data notebook and code for Gaussian initial data

--- MultiPatchWaveToy/src/gaussian.hxx
+++ MultiPatchWaveToy/src/gaussian.hxx
@@ -3,31 +3,151 @@
 
 namespace MultiPatchWaveToy::gaussian {
 
-template <typename T>
-static inline auto phi(T A, T gaussian_width, T t, T x, T y,
-                       T z) noexcept -> T {
-  return 0.0; // TODO
+Print[gaussString]
+
+template<typename T>
+static inline auto phi (T A, T kx, T ky, T kz, T t, T x, T y, T z) noexcept -> T {
+  using std::sqrt, std::cosh, std::sinh;
+  
+  const auto r{sqrt(x * x + y * y + z * z)};
+
+  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+    return (2*A*t)/(exp(pow(t,2)/(2.*pow(W,2)))*pow(W,2));
+  } else {
+    return (A*(exp(-0.5*pow(t - sqrt(pow(x,2) + pow(y,2) + pow(z,2)),2)/pow(W,2)) - exp(-0.5*pow(t + sqrt(pow(x,2) + pow(y,2) + pow(z,2)),2)/pow(W,2))))/sqrt(pow(x,2) + pow(y,2) + pow(z,2));
+  }
 }
 
-template <typename T>
-static inline auto Pi(T A, T gaussian_width, T t, T x, T y, T z) noexcept -> T {
-  return 0.0; // TODO
+template<typename T>
+static inline auto Pi (T A, T kx, T ky, T kz, T t, T x, T y, T z) noexcept -> T {
+  using std::sqrt, std::cosh, std::sinh;
+  
+  const auto r{sqrt(x * x + y * y + z * z)};
+
+  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+    return (2*A*(-t + W)*(t + W))/(exp(pow(t,2)/(2.*pow(W,2)))*pow(W,4));
+  } else {
+    return (2*A*(sqrt(pow(x,2) + pow(y,2) + pow(z,2))*cosh((t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)) - t*sinh((t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))))/(exp((pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2))/(2.*pow(W,2)))*pow(W,2)*sqrt(pow(x,2) + pow(y,2) + pow(z,2)));
+  }
 }
 
-template <typename T>
-static inline auto Dx(T A, T gaussian_width, T t, T x, T y, T z) noexcept -> T {
-  return 0.0; // TODO
+template<typename T>
+static inline auto Dx (T A, T kx, T ky, T kz, T t, T x, T y, T z) noexcept -> T {
+  using std::sqrt, std::cosh, std::sinh;
+  
+  const auto r{sqrt(x * x + y * y + z * z)};
+
+  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+    return 0;
+  } else {
+    return (A*x*(pow(W,2) + pow(x,2) + pow(y,2) + pow(z,2) + t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) - exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))*(pow(W,2) + pow(x,2) + pow(y,2) + pow(z,2))))/(exp((pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2) + 2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/(2.*pow(W,2)))*pow(W,2)*pow(pow(x,2) + pow(y,2) + pow(z,2),1.5));
+  }
 }
 
-template <typename T>
-static inline auto Dy(T A, T gaussian_width, T t, T x, T y, T z) noexcept -> T {
-  return 0.0; // TODO
+template<typename T>
+static inline auto Dy (T A, T kx, T ky, T kz, T t, T x, T y, T z) noexcept -> T {
+  using std::sqrt, std::cosh, std::sinh;
+  
+  const auto r{sqrt(x * x + y * y + z * z)};
+
+  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+    return 0;
+  } else {
+    return (A*y*(pow(W,2) + pow(x,2) + pow(y,2) + pow(z,2) + t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) - exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))*(pow(W,2) + pow(x,2) + pow(y,2) + pow(z,2))))/(exp((pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2) + 2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/(2.*pow(W,2)))*pow(W,2)*pow(pow(x,2) + pow(y,2) + pow(z,2),1.5));
+  }
 }
 
-template <typename T>
-static inline auto Dz(T A, T gaussian_width, T t, T x, T y, T z) noexcept -> T {
-  return 0.0; // TODO
+template<typename T>
+static inline auto Dz (T A, T kx, T ky, T kz, T t, T x, T y, T z) noexcept -> T {
+  using std::sqrt, std::cosh, std::sinh;
+  
+  const auto r{sqrt(x * x + y * y + z * z)};
+
+  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+    return 0;
+  } else {
+    return (A*z*(pow(W,2) + pow(x,2) + pow(y,2) + pow(z,2) + t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) - exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))*(pow(W,2) + pow(x,2) + pow(y,2) + pow(z,2))))/(exp((pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2) + 2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/(2.*pow(W,2)))*pow(W,2)*pow(pow(x,2) + pow(y,2) + pow(z,2),1.5));
+  }
 }
+
+template<typename T>
+static inline auto dPidx (T A, T kx, T ky, T kz, T t, T x, T y, T z) noexcept -> T {
+  using std::sqrt, std::cosh, std::sinh;
+  
+  const auto r{sqrt(x * x + y * y + z * z)};
+
+  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+    return 0;
+  } else {
+    return (-2*A*x*(sqrt(pow(x,2) + pow(y,2) + pow(z,2))*(pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2))*cosh((t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)) - t*(pow(W,2) + 2*(pow(x,2) + pow(y,2) + pow(z,2)))*sinh((t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))))/(exp((pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2))/(2.*pow(W,2)))*pow(W,4)*pow(pow(x,2) + pow(y,2) + pow(z,2),1.5));
+  }
+}
+
+template<typename T>
+static inline auto dPidy (T A, T kx, T ky, T kz, T t, T x, T y, T z) noexcept -> T {
+  using std::sqrt, std::cosh, std::sinh;
+  
+  const auto r{sqrt(x * x + y * y + z * z)};
+
+  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+    return 0;
+  } else {
+    return (-2*A*y*(sqrt(pow(x,2) + pow(y,2) + pow(z,2))*(pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2))*cosh((t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)) - t*(pow(W,2) + 2*(pow(x,2) + pow(y,2) + pow(z,2)))*sinh((t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))))/(exp((pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2))/(2.*pow(W,2)))*pow(W,4)*pow(pow(x,2) + pow(y,2) + pow(z,2),1.5));
+  }
+}
+
+template<typename T>
+static inline auto dPidz (T A, T kx, T ky, T kz, T t, T x, T y, T z) noexcept -> T {
+  using std::sqrt, std::cosh, std::sinh;
+  
+  const auto r{sqrt(x * x + y * y + z * z)};
+
+  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+    return 0;
+  } else {
+    return (-2*A*z*(sqrt(pow(x,2) + pow(y,2) + pow(z,2))*(pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2))*cosh((t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)) - t*(pow(W,2) + 2*(pow(x,2) + pow(y,2) + pow(z,2)))*sinh((t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))))/(exp((pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2))/(2.*pow(W,2)))*pow(W,4)*pow(pow(x,2) + pow(y,2) + pow(z,2),1.5));
+  }
+}
+
+template<typename T>
+static inline auto dDxdx (T A, T kx, T ky, T kz, T t, T x, T y, T z) noexcept -> T {
+  using std::sqrt, std::cosh, std::sinh;
+  
+  const auto r{sqrt(x * x + y * y + z * z)};
+
+  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+    return 0;
+  } else {
+    return (A*((-1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*pow(W,4)*(2*pow(x,2) - pow(y,2) - pow(z,2)) - 2*(1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*t*pow(x,2)*pow(pow(x,2) + pow(y,2) + pow(z,2),1.5) + (-1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*pow(x,2)*(pow(x,2) + pow(y,2) + pow(z,2))*(pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2)) + pow(W,2)*((-1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*pow(x,4) - 2*(1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*t*pow(x,2)*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) - (pow(y,2) + pow(z,2))*((-1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*pow(y,2) - pow(z,2) - t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))*(pow(z,2) - t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))))))/(exp((pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2) + 2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/(2.*pow(W,2)))*pow(W,4)*pow(pow(x,2) + pow(y,2) + pow(z,2),2.5));
+  }
+}
+
+template<typename T>
+static inline auto dDydy (T A, T kx, T ky, T kz, T t, T x, T y, T z) noexcept -> T {
+  using std::sqrt, std::cosh, std::sinh;
+  
+  const auto r{sqrt(x * x + y * y + z * z)};
+
+  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+    return 0;
+  } else {
+    return -((A*((-1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*pow(W,4)*(pow(x,2) - 2*pow(y,2) + pow(z,2)) - pow(y,2)*(pow(x,2) + pow(y,2) + pow(z,2))*((-1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*pow(t,2) - 2*(1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) + (-1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*(pow(x,2) + pow(y,2) + pow(z,2))) + pow(W,2)*((-1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*pow(x,4) + pow(y,4) - pow(z,4) + t*(2*pow(y,2) - pow(z,2))*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) + pow(x,2)*(2*(-1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*pow(z,2) - (1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2))) + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))*(-pow(y,4) + pow(z,4) + 2*t*pow(y,2)*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) - t*pow(z,2)*sqrt(pow(x,2) + pow(y,2) + pow(z,2))))))/(exp((pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2) + 2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/(2.*pow(W,2)))*pow(W,4)*pow(pow(x,2) + pow(y,2) + pow(z,2),2.5)));
+  }
+}
+
+template<typename T>
+static inline auto dDzdz (T A, T kx, T ky, T kz, T t, T x, T y, T z) noexcept -> T {
+  using std::sqrt, std::cosh, std::sinh;
+  
+  const auto r{sqrt(x * x + y * y + z * z)};
+
+  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+    return 0;
+  } else {
+    return -((A*((-1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*pow(W,4)*(pow(x,2) + pow(y,2) - 2*pow(z,2)) + 2*(1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*t*pow(z,2)*pow(pow(x,2) + pow(y,2) + pow(z,2),1.5) - (-1 + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2)))*pow(z,2)*(pow(x,2) + pow(y,2) + pow(z,2))*(pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2)) + pow(W,2)*(-pow(pow(x,2) + pow(y,2),2) + pow(z,4) - t*pow(x,2)*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) - exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))*t*pow(x,2)*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) - t*pow(y,2)*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) - exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))*t*pow(y,2)*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) + 2*t*pow(z,2)*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) + 2*exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))*t*pow(z,2)*sqrt(pow(x,2) + pow(y,2) + pow(z,2)) + exp((2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/pow(W,2))*(pow(pow(x,2) + pow(y,2),2) - pow(z,4)))))/(exp((pow(t,2) + pow(x,2) + pow(y,2) + pow(z,2) + 2*t*sqrt(pow(x,2) + pow(y,2) + pow(z,2)))/(2.*pow(W,2)))*pow(W,4)*pow(pow(x,2) + pow(y,2) + pow(z,2),2.5)));
+  }
+}
+
 
 } // namespace MultiPatchWaveToy::gaussian
 

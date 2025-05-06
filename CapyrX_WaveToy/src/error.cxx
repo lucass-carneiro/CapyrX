72019f78a16307d31460df39022ba03f36a68c08
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2025-05-06 17:03:55 -0500

    CapyrX_WaveToy: Updated schedule and standing wave par file

--- CapyrX_WaveToy/src/error.cxx
+++ CapyrX_WaveToy/src/error.cxx
@@ -16,7 +16,7 @@ extern "C" void CapyrX_WaveToy_Error(CCTK_ARGUMENTS) {
   using namespace Loop;
 
   if (CCTK_EQUALS(initial_condition, "standing wave")) {
-    grid.loop_int_device<0, 0, 0>(
+    grid.loop_all_device<0, 0, 0>(
         grid.nghostzones, [=] CCTK_HOST CCTK_DEVICE(
                               const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
           using std::fabs;

dc555801b8f60791ccebd47862d341072f8eef07
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2025-08-23 02:08:23 -0500

    CapyrX_WaveToy: Dissipation stencils

--- CapyrX_WaveToy/src/rhs.cxx
+++ CapyrX_WaveToy/src/rhs.cxx
@@ -1,3 +1,4 @@
+#include "cctk_Config.h"
 #include <cctk.h>
 #include <cctk_Arguments.h>
 #include <cctk_Parameters.h>
@@ -17,22 +18,34 @@ static inline auto CCTK_ATTRIBUTE_ALWAYS_INLINE CCTK_HOST CCTK_DEVICE
   return num * den;
 }
 
-static inline auto CCTK_ATTRIBUTE_ALWAYS_INLINE CCTK_HOST CCTK_DEVICE
-c4o_0_1_0(const Loop::PointDesc &p,
-          const Loop::GF3D2<const CCTK_REAL> &gf) noexcept -> CCTK_REAL {
+static inline auto CCTK_ATTRIBUTE_ALWAYS_INLINE CCTK_HOST CCTK_DEVICE c4o_0_1_0(
+    const Loop::PointDesc &p, const Loop::GF3D2<const CCTK_REAL> &gf) noexcept
+    -> CCTK_REAL {
   const auto num{gf(-2 * p.DI[1] + p.I) - 8 * gf(-p.DI[1] + p.I) +
                  8 * gf(p.DI[1] + p.I) - gf(2 * p.DI[1] + p.I)};
   const auto den{1.0 / (12 * p.DX[1])};
   return num * den;
 }
 
-static inline auto CCTK_ATTRIBUTE_ALWAYS_INLINE CCTK_HOST CCTK_DEVICE
-c4o_0_0_1(const Loop::PointDesc &p,
-          const Loop::GF3D2<const CCTK_REAL> &gf) noexcept -> CCTK_REAL {
+static inline auto CCTK_ATTRIBUTE_ALWAYS_INLINE CCTK_HOST CCTK_DEVICE c4o_0_0_1(
+    const Loop::PointDesc &p, const Loop::GF3D2<const CCTK_REAL> &gf) noexcept
+    -> CCTK_REAL {
   const auto num{gf(-2 * p.DI[2] + p.I) - 8 * gf(-p.DI[2] + p.I) +
                  8 * gf(p.DI[2] + p.I) - gf(2 * p.DI[2] + p.I)};
   const auto den{1.0 / (12 * p.DX[2])};
   return num * den;
+}
+
+template <std::size_t dir>
+static inline auto CCTK_ATTRIBUTE_ALWAYS_INLINE CCTK_DEVICE diss_5(
+    const Loop::PointDesc &p, const Loop::GF3D2<const CCTK_REAL> &gf) noexcept
+    -> CCTK_REAL {
+  const auto fac{(1.0 / 64.0) * (1.0 / p.DX[dir])};
+  const auto stencil{gf(p.I - 3 * p.DI[dir]) - 6.0 * gf(p.I - 2 * p.DI[dir]) +
+                     15.0 * gf(p.I - 1 * p.DI[dir]) - 20.0 * gf(p.I) +
+                     15.0 * gf(p.I + 1 * p.DI[dir]) -
+                     6.0 * gf(p.I + 2 * p.DI[dir]) + gf(p.I + 3 * p.DI[dir])};
+  return fac * stencil;
 }
 
 extern "C" void CapyrX_WaveToy_RHS(CCTK_ARGUMENTS) {
@@ -64,11 +77,31 @@ extern "C" void CapyrX_WaveToy_RHS(CCTK_ARGUMENTS) {
         const auto gDy{project_first(lDy, jac)};
         const auto gDz{project_first(lDz, jac)};
 
-        phi_rhs(p.I) = Pi(p.I);
-        Pi_rhs(p.I) = gDx.dx + gDy.dy + gDz.dz;
-        Dx_rhs(p.I) = gPi.dx;
-        Dy_rhs(p.I) = gPi.dy;
-        Dz_rhs(p.I) = gPi.dz;
+        const auto diss_phi{
+            dissipation_epsilon *
+            (diss_5<0>(p, phi) + diss_5<1>(p, phi) + diss_5<2>(p, phi))};
+
+        const auto diss_Pi{
+            dissipation_epsilon *
+            (diss_5<0>(p, Pi) + diss_5<1>(p, Pi) + diss_5<2>(p, Pi))};
+
+        const auto diss_Dx{
+            dissipation_epsilon *
+            (diss_5<0>(p, Dx) + diss_5<1>(p, Dx) + diss_5<2>(p, Dx))};
+
+        const auto diss_Dy{
+            dissipation_epsilon *
+            (diss_5<0>(p, Dy) + diss_5<1>(p, Dy) + diss_5<2>(p, Dy))};
+
+        const auto diss_Dz{
+            dissipation_epsilon *
+            (diss_5<0>(p, Dz) + diss_5<1>(p, Dz) + diss_5<2>(p, Dz))};
+
+        phi_rhs(p.I) = Pi(p.I) + diss_phi;
+        Pi_rhs(p.I) = gDx.dx + gDy.dy + gDz.dz + diss_Pi;
+        Dx_rhs(p.I) = gPi.dx + diss_Dx;
+        Dy_rhs(p.I) = gPi.dy + diss_Dy;
+        Dz_rhs(p.I) = gPi.dz + diss_Dz;
       });
 }
 

2405aa415c5482ddaa833834ec4dd19eb422f162
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2025-12-21 03:29:23 -0600

    CapyrX_MultiPatch: Added Thornburg-6 coordinates

--- CapyrX_WaveToy_SecondOrder/src/rhs.cxx
+++ CapyrX_WaveToy_SecondOrder/src/rhs.cxx
@@ -20,33 +20,24 @@ extern "C" void CapyrX_WaveToy_SecondOrder_RHS(CCTK_ARGUMENTS) {
   grid.loop_int_device<0, 0, 0>(
       grid.nghostzones,
       [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-        if (p.patch == 0) {
-          // No need to project for cartesian patches
-          const auto d2u_dx2 = d_xx<0>(p, u);
-          const auto d2u_dy2 = d_xx<1>(p, u);
-          const auto d2u_dz2 = d_xx<2>(p, u);
-          rho_rhs(p.I) = d2u_dx2 + d2u_dy2 + d2u_dz2;
-        } else {
-          // Non cartesian patches need projection
-          const LocalFirstDerivatives lu{
-              .da = d_x<0>(p, u), .db = d_x<1>(p, u), .dc = d_x<2>(p, u)};
+        const LocalFirstDerivatives lu{
+            .da = d_x<0>(p, u), .db = d_x<1>(p, u), .dc = d_x<2>(p, u)};
 
-          const LocalSecondDerivatives l2u{
-              .dada = d_xx<0>(p, u),
-              .dadb = d_xy<0, 1>(p, u),
-              .dadc = d_xy<0, 2>(p, u),
-              .dbdb = d_xx<1>(p, u),
-              .dbdc = d_xy<1, 2>(p, u),
-              .dcdc = d_xx<2>(p, u),
-          };
+        const LocalSecondDerivatives l2u{
+            .dada = d_xx<0>(p, u),
+            .dadb = d_xy<0, 1>(p, u),
+            .dadc = d_xy<0, 2>(p, u),
+            .dbdb = d_xx<1>(p, u),
+            .dbdc = d_xy<1, 2>(p, u),
+            .dcdc = d_xx<2>(p, u),
+        };
 
-          const Jacobians jac{VERTEX_JACOBIANS(p)};
-          const JacobianDerivatives djac{VERTEX_DJACOBIANS(p)};
+        const Jacobians jac{VERTEX_JACOBIANS(p)};
+        const JacobianDerivatives djac{VERTEX_DJACOBIANS(p)};
 
-          const auto d2ux{project_second(lu, l2u, jac, djac)};
+        const auto d2ux{project_second(lu, l2u, jac, djac)};
 
-          rho_rhs(p.I) = d2ux.dxdx + d2ux.dydy + d2ux.dzdz;
-        }
+        rho_rhs(p.I) = d2ux.dxdx + d2ux.dydy + d2ux.dzdz;
 
         u_rhs(p.I) = rho(p.I);
       });

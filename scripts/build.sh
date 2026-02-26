22b7966172f22dab5059f223c13183ecfeef7ece
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2026-02-26 16:25:45 -0600

    Capyrx: Attempt to CI 1

---
+++ scripts/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -ex
+
+export CAPYRXSPACE="$PWD"
+export WORKSPACE="$PWD/../workspace"
+mkdir -p "$WORKSPACE"
+cd "$WORKSPACE"
+
+cd Cactus
+
+# Set up SimFactory
+cp "$CAPYRXSPACE/scripts/actions-$ACCELERATOR-$REAL_PRECISION.cfg" ./simfactory/mdb/optionlists
+cp "$CAPYRXSPACE/scripts/actions-$ACCELERATOR-$REAL_PRECISION.ini" ./simfactory/mdb/machines
+cp "$CAPYRXSPACE/scripts/actions-$ACCELERATOR-$REAL_PRECISION.run" ./simfactory/mdb/runscripts
+cp "$CAPYRXSPACE/scripts/actions-$ACCELERATOR-$REAL_PRECISION.sub" ./simfactory/mdb/submitscripts
+cp "$CAPYRXSPACE/scripts/capyrx.th" .
+cp "$CAPYRXSPACE/scripts/defs.local.ini" ./simfactory/etc
+
+# For Formaline
+git config --global user.email "capyrx@einsteintoolkit.org"
+git config --global user.name "Github Actions"
+
+case "$MODE" in
+    debug) mode='--debug';;
+    optimize) mode='--optimise';;
+    *) exit 1;;
+esac
+
+# Build
+# The build log needs to be stored for later.
+time ./simfactory/bin/sim --machine="actions-$ACCELERATOR-$REAL_PRECISION" build "$mode" --jobs $(nproc) sim 2>&1 |
+    tee build.log
+
+# Check whether the executable exists and is executable
+test -x exe/cactus_sim
---
+++ scripts/test.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -ex
+
+export CAPYRXSPACE="$PWD"
+export WORKSPACE="$PWD/../workspace"
+export PAGESSPACE="$PWD/gh-pages"
+mkdir -p "$WORKSPACE"
+cd "$WORKSPACE"
+
+cd Cactus
+
+# Somehow /usr/local/lib is not in the search path
+export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
+# OpenMPI does not like to run as root (even in a container)
+export OMPI_ALLOW_RUN_AS_ROOT=1
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
+
+time ./simfactory/bin/sim --machine="actions-$ACCELERATOR-$REAL_PRECISION" create-run TestJob01_temp_1 --cores 1 --num-threads 2 --testsuite --select-tests=CapyrX_TestMultiPatch,CapyrX_WaveToy
+ONEPROC_DIR="$(./simfactory/bin/sim --machine="actions-$ACCELERATOR-$REAL_PRECISION" get-output-dir TestJob01_temp_1)/TEST/sim"
+
+time ./simfactory/bin/sim --machine="actions-$ACCELERATOR-$REAL_PRECISION" create-run TestJob01_temp_2 --cores 2 --num-threads 1 --testsuite --select-tests=CapyrX_TestMultiPatch,CapyrX_WaveToy
+TWOPROC_DIR="$(./simfactory/bin/sim --machine="actions-$ACCELERATOR-$REAL_PRECISION" get-output-dir TestJob01_temp_2)/TEST/sim"
+
+# Show all log, output, and error files
+echo 'All log files:'
+echo '================================================================================'
+for logfile in $(find "${ONEPROC_DIR}" "${TWOPROC_DIR}" -name '*.log' -print); do
+    echo "Log file $logfile:"
+    ls -l "$logfile"
+    cat "$logfile"
+    echo '================================================================================'
+done
+echo 'All output files:'
+echo '================================================================================'
+for outfile in $(find "${ONEPROC_DIR}/../.." "${TWOPROC_DIR}/../.." -name '*.out' -print); do
+    echo "Output file $outfile:"
+    ls -l "$outfile"
+    cat "$outfile"
+    echo '================================================================================'
+done
+echo 'All error files:'
+echo '================================================================================'
+for errfile in $(find "${ONEPROC_DIR}/../.." "${TWOPROC_DIR}/../.." -name '*.err' -print); do
+    echo "Error file $errfile:"
+    ls -l "$errfile"
+    cat "$errfile"
+    echo '================================================================================'
+done
+
+for test_dir in "${ONEPROC_DIR}" "${TWOPROC_DIR}"; do
+    log="${test_dir}/summary.log"
+    if ! grep -q '^    Number failed            -> 0$' ${log}; then
+        echo 'FAILURE'
+        exit 1
+    fi
+done
+echo 'SUCCESS'
+exit 0

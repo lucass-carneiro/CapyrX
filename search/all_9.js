6e280a9e20deb87265d6dd3c9c1ecc3141e47bc3
lucass-carneiro <lucas.t.s.carneiro@gmail.com>
2026-04-01 05:49:28 +0000

    Deploying to gh-pages from @ lucass-carneiro/CapyrX@f5eefdda1abfad00dece3b5dd61d68a052de4bf0 🚀

--- search/all_9.js
+++ search/all_9.js
@@ -3,7 +3,7 @@ var searchData=
   ['patch_0',['Patch',['../structCapyrX_1_1MultiPatch_1_1Patch.html',1,'CapyrX::MultiPatch']]],
   ['patches_1',['patches',['../structCapyrX_1_1MultiPatch_1_1PatchSystem.html#a35eb01d5acf73bf0a5b6cb6a771fb8e7',1,'CapyrX::MultiPatch::PatchSystem']]],
   ['patchface_2',['PatchFace',['../structCapyrX_1_1MultiPatch_1_1PatchFace.html',1,'CapyrX::MultiPatch']]],
-  ['patchparams_3',['patchparams',['../structCapyrX_1_1MultiPatch_1_1Cartesian_1_1PatchParams.html',1,'CapyrX::MultiPatch::Cartesian::PatchParams'],['../structCapyrX_1_1MultiPatch_1_1CubedSphere_1_1PatchParams.html',1,'CapyrX::MultiPatch::CubedSphere::PatchParams'],['../structCapyrX_1_1MultiPatch_1_1Thornburg06_1_1PatchParams.html',1,'CapyrX::MultiPatch::Thornburg06::PatchParams']]],
+  ['patchparams_3',['patchparams',['../structCapyrX_1_1MultiPatch_1_1Cartesian_1_1PatchParams.html',1,'CapyrX::MultiPatch::Cartesian::PatchParams'],['../structCapyrX_1_1MultiPatch_1_1CubedSphere_1_1PatchParams.html',1,'CapyrX::MultiPatch::CubedSphere::PatchParams'],['../structCapyrX_1_1MultiPatch_1_1Thornburg06_1_1PatchParams.html',1,'CapyrX::MultiPatch::Thornburg06::PatchParams'],['../structCapyrX_1_1MultiPatch_1_1TwoCubes_1_1PatchParams.html',1,'CapyrX::MultiPatch::TwoCubes::PatchParams']]],
   ['patchsystem_4',['PatchSystem',['../structCapyrX_1_1MultiPatch_1_1PatchSystem.html',1,'CapyrX::MultiPatch']]],
   ['point_5flocation_5',['point_location',['../structCapyrX_1_1MultiPatch_1_1point__location.html',1,'CapyrX::MultiPatch']]]
 ];

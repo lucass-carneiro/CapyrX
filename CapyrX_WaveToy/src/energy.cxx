42b2bf2b1e2bf084027c966e0fd626ce7522fdda
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2026-05-27 15:52:24 -0500

    CapyrX_WaveToy: Corrected the wave energy computation

--- CapyrX_WaveToy/src/energy.cxx
+++ CapyrX_WaveToy/src/energy.cxx
@@ -3,6 +3,7 @@
 #include <cctk_Parameters.h>
 
 #include <loop_device.hxx>
+#include <mat.hxx>
 
 namespace CapyrX::WaveToy {
 
@@ -11,18 +12,25 @@ extern "C" void CapyrX_WaveToy_Energy(CCTK_ARGUMENTS) {
   DECLARE_CCTK_PARAMETERS;
 
   using namespace Loop;
+  using namespace Arith;
 
   grid.loop_all_device<0, 0, 0>(
-      grid.nghostzones, [=] CCTK_HOST CCTK_DEVICE(const PointDesc &p)
-                            CCTK_ATTRIBUTE_ALWAYS_INLINE {
-                              const auto l_Pi{Pi(p.I)};
-                              const auto l_Dx{Dx(p.I)};
-                              const auto l_Dy{Dy(p.I)};
-                              const auto l_Dz{Dz(p.I)};
+      grid.nghostzones, [=] CCTK_HOST CCTK_DEVICE(
+                            const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        const smat<CCTK_REAL, 3> g_dd{gxx(p.I), gxy(p.I), gxz(p.I),
+                                      gyy(p.I), gyz(p.I), gzz(p.I)};
+        const auto detg = calc_det(g_dd);
+        const auto sqrtdetg = sqrt(detg);
+        const auto g_uu = calc_inv(g_dd, detg);
 
-                              energy(p.I) = 0.5 * (l_Pi * l_Pi + l_Dx * l_Dx +
-                                                   l_Dy * l_Dy + l_Dz * l_Dz);
-                            });
+        energy(p.I) = 0.5 * sqrtdetg *
+                      (Pi(p.I) * Pi(p.I) + g_uu(0, 0) * Dx(p.I) * Dx(p.I) +
+                       2.0 * g_uu(0, 1) * Dx(p.I) * Dy(p.I) +
+                       2.0 * g_uu(0, 2) * Dx(p.I) * Dz(p.I) +
+                       g_uu(1, 1) * Dy(p.I) * Dy(p.I) +
+                       2.0 * g_uu(1, 2) * Dy(p.I) * Dz(p.I) +
+                       g_uu(2, 2) * Dz(p.I) * Dz(p.I));
+      });
 }
 
 } // namespace CapyrX::WaveToy
